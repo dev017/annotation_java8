1613faa56737249623fcde4770d8afa6186499c3
dev017 <dev017@192.168.1.3>
2017-04-09 00:40:30 +0100

    Créer une annotation avec Java 8

---
+++ src/annotation_java8/Main.java
@@ -0,0 +1,10 @@
+package annotation_java8;
+
+public class Main {
+
+	 public static void main(String[] args) {
+	       
+	        Voiture v = new Voiture();
+	        
+	    }
+}
---
+++ src/annotation_java8/Voiture.java
@@ -0,0 +1,15 @@
+package annotation_java8;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+@Documentation (auteur = "Ihab",date="01/01/2017",version=2, responsables={"Ihab","AAA"})
+@Documentation (auteur = "Ihab",date="01/01/2017",version=2, responsables={"Ihab","AAA"})
+public class Voiture {
+    
+    //@Documentation (auteur = "Ihab",date="01/01/2017",version=2, responsables={"Ihab","AAA"})
+    void methode() {}
+}
